2198cd9e74c5a43635e0dbf6ccae02c7dc2f53d3
Ricky Leung <ricxx0320@gmail.com>
2022-01-03 01:34:04 -0800

    added routes and connect db

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'password',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
 
 // app.get('/', (req, res) => {
 //   res.json({
@@ -12,10 +25,16 @@ app.use(express.json());
 //   });
 // });
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
+
 //Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
